62d4734fa996d36ca1174e36902bd1cbb3183de9
Shadrack Ankomahene <ankomaheneshadrack@gmail.com>
2026-07-03 19:51:03 +0000

    chore(release): bump version to 1.1.5 and update release notes
    adjust the sales features reporting entry in the release notes

--- release/release-config.js
+++ release/release-config.js
@@ -8,13 +8,13 @@
 
 module.exports = {
   // Version information
-  version: "1.1.4",
+  version: "1.1.5",
 
   // Release notes in markdown format
   releaseNotes: `### Added
   
 ### Improved / Changed
-- Sale financial reporting
+- Sales features enhancements
 
 ### Bug Fixes
 `,
